43298e082ca05a7ebfc8998b8f8422140a74b058
Breno Ramalho Lemes <breno@br-lemes.net>
2020-01-28 20:16:16 -0400

    MIME types

---
+++ mime.lua
@@ -0,0 +1,10 @@
+
+local html = "text/html; charset=UTF-8"
+local json = "application/json; charset=UTF-8"
+local text = "text/plain; charset=UTF-8"
+
+return {
+	html       = html,
+	json       = json,
+	text       = text,
+}
