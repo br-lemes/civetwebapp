028bfa32d7823a32b62d79c7eb3afaccb9052fbc
Breno Ramalho Lemes <breno@br-lemes.net>
2020-04-12 06:21:25 -0400

    Don't show variables that don't exist and set style and script to block

--- boilerplate.lua
+++ boilerplate.lua
@@ -3,17 +3,25 @@ local mustache = require("mustache")
 
 local template = [[
 <!DOCTYPE html>
-<html lang="{{lang}}">
+<html{{#lang}} lang="{{lang}}"{{/lang}}>
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link href="/w3.css" rel="stylesheet">
-	<link rel="icon" href="/fatcow/16/{{favicon}}.png">
-	<title>{{title}}</title>
-	<style>{{{style}}}</style>
-	<script>{{{script}}}</script>
+	{{#favicon}}<link rel="icon" href="/fatcow/16/{{favicon}}.png">
+	{{/favicon}}<title>{{title}}</title>
+{{#style}}
+	<style>
+{{{style}}}
+	</style>
+{{/style}}
+{{#script}}
+	<script>
+{{{script}}}
+	</script>
+{{/script}}
 </head>
-<body class="{{body}}">
+<body{{#body}} class="{{body}}"{{/body}}>
 {{{content}}}
 </body>
 </html>
